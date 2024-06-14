fa8d659e13e4b3aeef4d221648840089e4624e5b
DavidV1600 <115104357+DavidV1600@users.noreply.github.com>
2024-06-14 21:11:33 +0300

    Update Simplu_NN.py

--- Simplu_NN.py
+++ Simplu_NN.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[24]:
 
 
 import os
@@ -25,7 +25,7 @@ from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
 
 
-# In[3]:
+# In[25]:
 
 
 def citeste_imagini_din_folder(folder):
@@ -40,7 +40,7 @@ def citeste_imagini_din_folder(folder):
     return imagini, nume_imagini
 
 
-# In[5]:
+# In[26]:
 
 
 train_df = pd.read_csv('train.csv')
@@ -48,7 +48,7 @@ validation_df = pd.read_csv('validation.csv')
 test_df = pd.read_csv('test.csv')
 
 
-# In[6]:
+# In[27]:
 
 
 # Dictionare ca sa retin pentru fiecare id al imaginii label-ul corect
@@ -57,7 +57,7 @@ validation_dict = validation_df.set_index('image_id')['label'].to_dict()
 # test_dict = test_df.set_index('image_id')['label'].to_dict()
 
 
-# In[7]:
+# In[28]:
 
 
 train_images, train_images_names = citeste_imagini_din_folder('train')
@@ -65,7 +65,7 @@ validation_images, validation_images_names = citeste_imagini_din_folder('validat
 test_images, test_images_names = citeste_imagini_din_folder('test')
 
 
-# In[8]:
+# In[29]:
 
 
 #scot .png ca sa fie acelasi format ca in submission.csv
@@ -74,7 +74,7 @@ validation_images_names = [x.replace('.png','') for x in validation_images_names
 test_images_names = [x.replace('.png','') for x in test_images_names]
 
 
-# In[9]:
+# In[30]:
 
 
 # retin in X - imaginile si in Y - label-urile
@@ -86,7 +86,7 @@ X_test = test_images
 print(y_train[:10])
 
 
-# In[10]:
+# In[31]:
 
 
 #le dau rezise sa devina vector (1d) ca sa le pot pune ca input in NN
@@ -95,21 +95,21 @@ X_val = torch.as_tensor(np.array(X_val), dtype=torch.float32).reshape(-1, 19200)
 X_test = torch.as_tensor(np.array(X_test), dtype=torch.float32).reshape(-1, 19200)
 
 
-# In[11]:
+# In[32]:
 
 
 print(X_train.dtype)
 
 
-# In[12]:
+# In[39]:
 
 
 #fac variabilele in care retin perechi de tipul (imagine-label) ca sa le pot pune in DataLoader
 train_data = [(img, label) for img,label in zip(X_train,y_train)]
-val_data = [(img, label) for img,label in zip(X_val,y_val)]
+validare_data = [(img, label) for img,label in zip(X_val,y_val)]
 
 
-# In[13]:
+# In[40]:
 
 
 plt.hist(y_train, bins=5)
@@ -122,15 +122,15 @@ plt.hist(y_train, bins=5)
 
 
 
-# In[14]:
+# In[47]:
 
 
 #fac dataloader din ele
-train_dataloader = DataLoader(train_data, batch_size=64)
-val_dataloader = DataLoader(val_data, batch_size=64)
+dataloader_train = DataLoader(train_data, batch_size=64)
+dataloader_validare = DataLoader(validare_data, batch_size=64)
 
 
-# In[16]:
+# In[42]:
 
 
 class NN(nn.Module):
@@ -141,115 +141,103 @@ class NN(nn.Module):
         self.f3 = nn.Linear(1280, 640)
         self.f34 = nn.Linear(640, 320)
         self.f4 = nn.Linear(320, 160)
+        self.n1 = nn.BatchNorm1d(1920)
+        self.n2 = nn.BatchNorm1d(1280)
+        self.n3 = nn.BatchNorm1d(640)
+        self.n34 = nn.BatchNorm1d(320)
+        self.n4 = nn.BatchNorm1d(160)
         self.output = nn.Linear(160, 3)
         self.dropout = nn.Dropout(0.5)
-        self.norm1 = nn.BatchNorm1d(1920)
-        self.norm2 = nn.BatchNorm1d(1280)
-        self.norm3 = nn.BatchNorm1d(640)
-        self.norm34 = nn.BatchNorm1d(320)
-        self.norm4 = nn.BatchNorm1d(160)
         
     def forward(self, x):
-        x = F.relu(self.norm1(self.f1(x)))
+        x = F.relu(self.n1(self.f1(x)))
         x = self.dropout(x)
-        x = F.relu(self.norm2(self.f2(x)))
+        x = F.relu(self.n2(self.f2(x)))
         x = self.dropout(x)
-        x = F.relu(self.norm3(self.f3(x)))
+        x = F.relu(self.n3(self.f3(x)))
         x = self.dropout(x)
-        x = F.relu(self.norm34(self.f34(x)))
+        x = F.relu(self.n34(self.f34(x)))
         x = self.dropout(x)
-        x = F.relu(self.norm4(self.f4(x)))
+        x = F.relu(self.n4(self.f4(x)))
         x = self.dropout(x)
         x = self.output(x)
         return x
 
 
-# In[17]:
+# In[43]:
 
 
 model = NN()
 
 
-# In[23]:
+# In[48]:
 
 
-optimizator = optim.Adam(model.parameters(), lr=0.001)
 criteriu_loss = nn.CrossEntropyLoss()
+optimizator = optim.Adam(model.parameters(), lr=0.001)
+
 NR_EPOCI = 50
 for nr_epoca in range(NR_EPOCI):
-    print(f"Epoca: {nr_epoca}:")
-    
+    print(f"---{nr_epoca}---")
     model.train()
     predictii_corecte_epoca = 0
     nr_predictii_epoca = 0
     loss_epoca = 0.0
-    for batch, (batch_imagini, batch_labels) in enumerate(train_dataloader):
+    
+    for batch_imagini, batch_labels in dataloader_train:
         optimizator.zero_grad()
         predictie_batch = model(batch_imagini)
         loss_batch = criteriu_loss(predictie_batch, batch_labels)
-        loss_batch.backward()
         
+        loss_batch.backward()
         optimizator.step()
         loss_epoca += loss_batch.item()
         
-        _, predictie = torch.max(predictie_batch, 1)
+        predictie = predictie_batch.argmax(dim=1)
         predictii_corecte_epoca += (predictie == batch_labels).sum().item()
-        nr_predictii_epoca += batch_labels.size(0)
-        
-        if batch % 30 == 0:
-            print(f"Batch {batch} loss: {loss_batch.item():>6f}")
-    loss_epoca = loss_epoca / len(train_dataloader)
+        nr_predictii_epoca += batch_labels.shape[0]
+
     acurate_epoca = (predictii_corecte_epoca / nr_predictii_epoca) * 100
-    print(f"Epoca {nr_epoca}, Train Loss: {loss_epoca:.3f}, Train acuratete: {acurate_epoca:.2f}%")
+    loss_epoca = loss_epoca / len(dataloader_train)
+    print(f"Train Loss: {loss_epoca}")
+    print(f"Train Acc: {acurate_epoca}%")
     
     model.eval()
     nr_predictii_corecte_validare = 0
     nr_predictii_totale_validare = 0
     loss_validare = 0.0
+
     with torch.no_grad():
-        for batch_imagini, batch_labels in val_dataloader:            
-            predictie = model(batch_imagini)
-            loss = criteriu_loss(predictie, batch_labels)
+        for imagini_validare, labels_validare in dataloader_validare:            
+            predictie = model(imagini_validare)
+            loss = criteriu_loss(predictie, labels_validare)
             loss_validare += loss.item()
             
-            _, predictie = torch.max(predictie, 1)
-            nr_predictii_corecte_validare += (predictie == batch_labels).sum().item()
-            nr_predictii_totale_validare += batch_labels.size(0)
-            
+            predictie = predictie.argmax(dim=1)
+            nr_predictii_corecte_validare += (predictie == labels_validare).sum().item()
+            nr_predictii_totale_validare += labels_validare.shape[0]
+    
+    loss_validare = loss_validare / len(dataloader_validare)
     acuratete_validare = 100 * (nr_predictii_corecte_validare / nr_predictii_totale_validare)
-    loss_validare = loss_validare / len(val_dataloader)
-
-    print(f"Validare loss: {loss_validare:.3f}, Validare acuratete: {acuratete_validare:.2f}%")
+    print(f"Validare loss: {loss_validare}")
+    print(f"Validare Acc: {acuratete_validare}%")
 
 
 # In[ ]:
 
 
-predictii = []
-model.eval()
+predictii_finale = []
+model2.eval()
 with torch.no_grad():
-    for imagini, nume_imagini in test_dataloader:
-        predictii = model(imagini)
-        _, preds = torch.max(predictii, 1)
-        for filename, pred in zip(nume_imagini, preds.cpu().numpy()):
-            predictii.append((filename, pred))
+    for imagini, nume_imagini in dataloader_test:
+        predictii = model2(imagini)
+        predictii = predictii.argmax(dim=1)
+        for nume_imagine, predictie in zip(nume_imagini, predictii.numpy()):
+            predictii_finale.append((nume_imagine, predictie))
 
 
 # In[23]:
 
 
-predictii_df = pd.DataFrame(predictii, columns=['image_id', 'label'])
+predictii_df = pd.DataFrame(predictii_finale, columns=['image_id', 'label'])
 predictii_df.to_csv('test_predictions_FULL_CNN_INCEPTION_SE.csv', index=False)
-
-
-# In[24]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
